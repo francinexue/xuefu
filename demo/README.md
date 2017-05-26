d18e41c0ef2a70f484a5d59bb3b5aa86a180e608
root <root123>
2017-05-26 23:01:59 +0800

    1.0

---
+++ demo/README.md
@@ -0,0 +1,18 @@
+## *示例集合* ##
+>- 以可能进行测试所需要的功能点划分，根据编写策略可能出现的编程难点一一作示例
+>- 每个示例里面的策略内容不是重点，当然也可以参考，15年写的版本比较粗糙
+
+### backtest文件夹 ###
+>- 回测示例
+- csvDemo:从csv读取数据并进行回测，策略为一不完善的海龟交易法
+- pandasDemo:**使pyalgotrade支持pandas的示例，并包含从tushare直接读取数据和从csv读入到pandas两种方式**
+  提供两种数据调取方式，一种为系统自带画图，另一种提供array方式回测结果的各数据的接口，详见cn.lib.pyalg_utils.py,添加运行时数据信息，格式为dic格式，包含return、sharpratio、tradeInfo等
+  ***注意：此方法只为监测数据并返回array，json等格式自己作图用。pyalgotrade本身已带作图方法及基础的信息。若不需要可删除调用部分：pyalg_util.py，pyalgo_test.py中的addInfo 方法，调用部分、getDateTimeSeries方法部分。***
+- sqlDemo:从数据库 postgres中读取或mongodb中读取两个示例
+- porfolioDemo:多股票或合约回测策略，示例代码为简单的动量策略
+- indexBBcurveDemo:指数择时策略示例，根据丁鹏-<量化投资策略与技术>书中描述的依据布朗运动计算牛熊线，判断大盘指数，并作为择时操作的依据，牛市、熊市、震荡市策略各有不同
+此处择时买卖策略仅是简单的超过牛线则买入，跌破熊线则卖出
+- minuWithDayDemo:日线和分钟线混合回测的例子，即本来使用5分钟线进行测试，但是中间还应用日线的各种指标，比如突破NN 日均线等
+本例子是通过计算ENE轨道，对每日k线突破上轨，中轨，下轨等不同情形进行运算
+- talibDemo:ta-lib示例，示例包含使用原生talib,pyalgotrade自带talib，调用自己写的util.formular中的公式,策略代码包含kdj aroon adx 3套策略示例
+
---
+++ demo/backtest/sqlDemo.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 06 11:13:33 2015
+从postgres中读取或mongodb中读取
+提供两种数据调取方式，一种为系统自带画图，另一种提供array方式各数据的接口，详见cn.lib.pyalg_utils.py
+@author: lenovo
+"""
+
+from matplotlib.pyplot import plot
+from pyalgotrade import plotter
+from pyalgotrade.stratanalyzer import returns
+
+import pandasDemo_run as pdr
+from quant.cn.lib import pyalg_utils, dataFramefeed
+
+
+def turtle_test(loadtype='pgs', dataString='pyalg'):
+    """
+    :param dataString:
+    :return:
+    """
+    # 从postgres 中加载
+    if loadtype == 'pgs':
+        from api.stock.histmd import to_postgres_md as tpd
+        dat = tpd.get_h_data('600848')
+    else:
+        from api.stock.histmd.to_mongodb_md import tmd
+        w = tmd.KdataDbCache()
+        dat = w.get_k_data('300426', start='2015-02-05', end='2015-02-19')
+    feed = dataFramefeed.Feed()
+    feed.addBarsFromDataFrame("orcl", dat)
+    myStrategy = pdr.turtle(feed, "orcl", 20, 10)
+    # Attach a returns analyzers to the strategy.
+    returnsAnalyzer = returns.Returns()
+    myStrategy.attachAnalyzer(returnsAnalyzer)
+
+    # Attach the plotter to the strategy.
+    plt = plotter.StrategyPlotter(myStrategy)
+    # Plot the simple returns on each bar.
+    plt.getOrCreateSubplot("returns").addDataSeries("Simple returns", returnsAnalyzer.getReturns())
+
+    if dataString == 'pyalg_util':
+        ds = pyalg_utils.dataSet(myStrategy)  # 抽取交易数据集语句，若使用系统自带画图功能则不需要该项
+    myStrategy.run()
+    myStrategy.info("Final portfolio value: $%.2f" % myStrategy.getResult())
+
+    if dataString == 'pyalg_util':
+        rs = ds.getDefault()  # 获取默认的交易信息，dic格式
+        plot(rs["cumulativeReturns"][:, 0], rs["cumulativeReturns"][:, 1])  # 简单作图示例
+
+    plt.plot()
+
+
+if __name__ == '__main__':
+    turtle_test()
